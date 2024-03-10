8cf51f442b719f66df49a30a04130db9605c089e
NipunikaRanasingha1993 <nipuniranasinghe1993@gmail.com>
2024-03-10 22:02:34 +0530

    fourth commit

--- src/main/java/lk/acpt/riyapola/repo/CarRepo.java
+++
@@ -1,7 +0,0 @@
-package lk.acpt.riyapola.repo;
-
-import lk.acpt.riyapola.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepo extends JpaRepository<Car, Integer> {
-}
