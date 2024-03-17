7b2c4506016c95081eafd0b4cb7d3a3943f58d84
NipunikaRanasingha1993 <nipuniranasinghe1993@gmail.com>
2024-03-17 05:59:27 +0530

    seventh commit

--- src/main/java/lk/acpt/riyapola/controller/CarController.java
+++ src/main/java/lk/acpt/riyapola/controller/CarController.java
@@ -4,6 +4,7 @@ import lk.acpt.riyapola.dto.CarDetailsGetDto;
 import lk.acpt.riyapola.dto.CarDto;
 import lk.acpt.riyapola.entity.Car;
 import lk.acpt.riyapola.service.CarService;
+import lk.acpt.riyapola.util.JWTTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +19,23 @@ import java.util.List;
 @CrossOrigin
 public class CarController {
     private final CarService carService;
+    private final JWTTokenGenerator jwtTokenGenerator;
 
     @Autowired
-    public CarController(CarService carService) {
+    public CarController(CarService carService, JWTTokenGenerator jwtTokenGenerator) {
         this.carService = carService;
+        this.jwtTokenGenerator = jwtTokenGenerator;
     }
 
     @PostMapping("/addNewCar")
-    public ResponseEntity<Object> saveCar(@ModelAttribute CarDto carDto) throws IOException, URISyntaxException {
-        CarDetailsGetDto dto = carService.saveCar(carDto);
-        return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    public ResponseEntity<Object> saveCar(@RequestHeader(name = "Authorization") String authorizationHeader,@ModelAttribute CarDto carDto) throws IOException, URISyntaxException {
+        if (this.jwtTokenGenerator.validateJwtToken(authorizationHeader)) {
+            CarDetailsGetDto dto = carService.saveCar(carDto);
+            return new ResponseEntity<>(dto, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("invalied token",HttpStatus.FORBIDDEN);
+        }
+
     }
     @GetMapping
     public ResponseEntity<List<Car>> getAllCars(){
